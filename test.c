8052b42498eacb85f0f391a786087759b6379061
Matt Davis <mattdavis9@gmail.com>
2017-03-21 21:07:59 -0700

    Add a test case.

--- test.c
+++ test.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-char *good = "I am spelled well.";
-char *bad = "I am noooot spelled well.";
+char *good       = "I am spelled well.";
+char *notgood    = "I am blajdlkj.";
+const char bad[] = "I am noooot spelled well."; // This will fail
 
 int main(void)
 {
-    char  literal1[] = "This is misssspelled literal";
-    char *literal2   = "This is another misssspelled literal";
+    char  literal1[] = "This is misssspelled literal"; // This will fail
+    char *literal2   = "This is another misssspelled literal"; // This will fail
     char *literal3   = "This is spelled perfectly!";
-    printf("This is a crapppily spelled string literal\n");
-    printf("%s\n", "hello");
+    printf("This is a crapppily spelled string literal\n"); // This will fail
+    printf("%s\n", bad); // This will fail
     return 0;
 }
